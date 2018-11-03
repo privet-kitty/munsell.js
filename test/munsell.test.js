0ddb3142a44c872860f03a798540d4d48d6d2f06
privet-kitty <privet.kitty99@gmail.com>
2018-11-03 21:11:28 +0900

    Define an extra matcher for Jest

--- test/munsell.test.js
+++ test/munsell.test.js
@@ -1,8 +1,8 @@
 import * as Munsell from '../munsell.js'
+import './jest_extension.js'
 
 describe('munsell', () => {    
-    test('calcMHVCToLCHabAllIntegerCase', () => {
-        expect(Munsell.calcMHVCToLCHabAllIntegerCase(0, 0, 0, true)).toEqual([0, 0, 0]);
-        })
-
+  test('calcMHVCToLCHabAllIntegerCase', () => {
+    expect([5, 4.0001]).toNearlyEqual([5, 4], 3);
+  })
 })
