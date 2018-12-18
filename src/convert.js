4d07ce273dbffd562df4627e0d1d8f8f1a3cee9e
privet-kitty <privet.kitty99@gmail.com>
2018-12-18 23:54:38 +0100

    Release version 0.1.3

--- src/convert.js
+++ src/convert.js
@@ -20,8 +20,8 @@ import {mod,
 /**
  * @name mhvc
  * @global
- * @desc MHVC, or Munsell HVC, is a 3-number expression of Munsell Color
- * composed of [Hue, Value, Chroma]: e.g. <code>[94.2, 3.5, 11]</code>,
+ * @desc <code>mhvc</code>, or Munsell HVC, is a 3-number expression of Munsell
+ * Color composed of [Hue, Value, Chroma]: e.g. <code>[94.2, 3.5, 11]</code>,
  * <code>[0, 10 ,0]</code>. Here Hue is in the circle group R/100Z: i.e. 0R (=
  * 10RP) corresponds to 0 (= 100 = 300 = -2000) and 2YR corresponds to 12 (= -88
  * = 412). Value is in the interval [0, 10] and the converters will clamp it if
